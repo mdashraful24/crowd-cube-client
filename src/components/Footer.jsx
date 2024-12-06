a6084117feac0140bf6a93eeb129723edb521092
mdashraful24 <mdashrafulislam2882@gmail.com>
2024-12-06 14:46:30 +0600

    added footer section done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -29,14 +29,16 @@ const Footer = () => {
                     <p>
                         <span className="text-3xl font-extrabold bg-gradient-to-r from-[#843c94] via-[#5e1e70] to-[#f97316] text-transparent bg-clip-text tracking-wide">CrowdCube</span>
                         <br />
-                        Innovating solutions for the future since 1992
+                        Innovating solutions for the future since 2000.
                         {/* Copyright */}
-                        <p>&copy; 2024, All rights reserved.</p>
+                        <p>&copy; {new Date().getFullYear()} CrowdCube. All rights reserved.</p>
                     </p>
                 </aside>
                 <nav className="md:place-self-center md:justify-self-end mt-2">
                     <div className="grid grid-flow-col gap-4">
-                        <a>
+                        <a
+                            href='https://www.facebook.com/ashraful.islam.ratul.455820?mibextid=ZbWKwL'
+                            target="_blank">
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="24"
@@ -47,7 +49,9 @@ const Footer = () => {
                                     d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
                             </svg>
                         </a>
-                        <a>
+                        <a
+                            href="https://x.com/?lang=en"
+                            target="_blank">
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="24"
@@ -58,10 +62,12 @@ const Footer = () => {
                                     d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
                             </svg>
                         </a>
-                        <a>
+                        <a href="https://www.instagram.com/" target="_blank">
                             <img className='w-8 h-6' src={instagram} alt="" />
                         </a>
-                        <a>
+                        <a
+                            href="https://www.youtube.com/results?search_query=crowedCube"
+                            target="_blank">
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="24"
@@ -76,7 +82,6 @@ const Footer = () => {
                 </nav>
             </footer>
         </div>
-
     );
 };
 
