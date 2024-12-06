9191869a664e4823e2d6ae97bf18e15c7ed2ac6c
mdashraful24 <mdashrafulislam2882@gmail.com>
2024-12-06 15:01:20 +0600

    added new second section with two react packages

--- src/components/SuccessStories.jsx
+++ src/components/SuccessStories.jsx
@@ -10,14 +10,13 @@ const SuccessStories = () => {
                 <Fade direction="up" triggerOnce>
                     <h2 className="text-3xl md:text-4xl text-[#5c0c9e] font-bold text-center mb-8">Why Choose CrowdCube?</h2>
                 </Fade>
-
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
                     {/* Typewriter Effect */}
                     <div className="text-center md:text-left p-6 shadow-lg rounded-lg border border-[#5c0c9e]">
                         <Fade direction="left" triggerOnce>
-                            <h3 className="text-xl text-[#72538b] font-semibold mb-4">
+                            <h3 className="text-xl font-semibold mb-4">
                                 <Typewriter
-                                    words={["Launch Your Vision", "Back a Dream", "Unite for a Cause", "Make an Impact", "Support a Movement", "Empower Change", "Realize Your Ambitions", "Shape the Future", "Achieve Your Goal", "Bring Ideas to Life"]}
+                                    words={["Launch Your Vision 🚀", "Back a Dream 🌟", "Unite for a Cause 🤝", "Make an Impact 🌍", "Support a Movement 📣", "Empower Change⚡", "Realize Your Ambitions 🎯", "Shape the Future 🔮", "Achieve Your Goal 🏆", "Bring Ideas to Life💡"]}
                                     loop={5}
                                     cursor
                                     cursorStyle="|"
@@ -28,7 +27,7 @@ const SuccessStories = () => {
                             </h3>
                         </Fade>
                         <Fade direction="left" triggerOnce>
-                            <p className="text-lg">
+                            <p className="text-lg text-justify">
                                 CrowdCube enables you to raise funds for your projects by reaching out to a large number of people. It's a great way to turn ideas into reality, gain exposure, and engage your community.
                             </p>
                         </Fade>
