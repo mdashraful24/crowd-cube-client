8eb2dedd4e1c79b66c4c9376d5289d419f8dc808
mdashraful24 <mdashrafulislam2882@gmail.com>
2024-12-07 18:04:05 +0600

    added why choose section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import instagram from '../../src/assets/instagram.png'
 
 const Footer = () => {
     return (
-        <div className="pb-2 shadow-inner border-t">
+        <div className="pb-2 shadow-inner border-t border-[#5c0c9e]">
             <footer className="footer container mx-auto md:justify-between lg:justify-evenly pt-10 px-8">
                 <nav>
                     <h6 className="text-lg uppercase font-bold">Services</h6>
@@ -44,11 +44,12 @@ const Footer = () => {
                                 width="24"
                                 height="24"
                                 viewBox="0 0 24 24"
-                                className="fill-current">
+                                className="fill-current transition-transform duration-500 ease-in-out hover:scale-125">
                                 <path
                                     d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
                             </svg>
                         </a>
+
                         <a
                             href="https://x.com/?lang=en"
                             target="_blank">
@@ -57,13 +58,13 @@ const Footer = () => {
                                 width="24"
                                 height="24"
                                 viewBox="0 0 24 24"
-                                className="fill-current">
+                                className="fill-current transition-transform duration-500 ease-in-out hover:scale-125">
                                 <path
                                     d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
                             </svg>
                         </a>
                         <a href="https://www.instagram.com/" target="_blank">
-                            <img className='w-8 h-6' src={instagram} alt="" />
+                            <img className='w-8 h-6 transition-transform duration-500 ease-in-out hover:scale-125' src={instagram} alt="" />
                         </a>
                         <a
                             href="https://www.youtube.com/results?search_query=crowedCube"
@@ -73,7 +74,7 @@ const Footer = () => {
                                 width="24"
                                 height="24"
                                 viewBox="0 0 24 24"
-                                className="fill-current">
+                                className="fill-current transition-transform duration-500 ease-in-out hover:scale-125">
                                 <path
                                     d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
                             </svg>
--- src/components/SuccessStories.jsx
+++ src/components/SuccessStories.jsx
@@ -6,23 +6,21 @@ const SuccessStories = () => {
     return (
         <section className="mt-20 mb-28">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                {/* Title effect */}
                 <Fade direction="up" triggerOnce>
                     <h2 className="text-3xl md:text-4xl text-purple-700 font-bold text-center mb-10">Why Choose CrowdCube?</h2>
                 </Fade>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-                    {/* Typewriter Effect */}
                     <div className="text-center md:text-left p-6 shadow-lg rounded-lg border border-[#5c0c9e]">
                         <Fade direction="left" triggerOnce>
                             <h3 className="text-xl text-red-500 font-semibold mb-4">
                                 <Typewriter
                                     words={["Launch Your Vision 🚀", "Back a Dream 🌟", "Unite for a Cause 🤝", "Make an Impact 🌍", "Support a Movement 📣", "Empower Change⚡", "Realize Your Ambitions 🎯", "Shape the Future 🔮", "Achieve Your Goal 🏆", "Bring Ideas to Life💡"]}
-                                    loop={5}
+                                    loop={0}
                                     cursor
                                     cursorStyle="|"
-                                    typeSpeed={100}
-                                    deleteSpeed={50}
-                                    delaySpeed={1500}
+                                    typeSpeed={80}
+                                    deleteSpeed={60}
+                                    delaySpeed={1200}
                                 />
                             </h3>
                         </Fade>
@@ -36,7 +34,7 @@ const SuccessStories = () => {
                         <Fade direction="right" triggerOnce>
                             <h3 className="text-xl font-semibold mb-4">What Are The Benefits?</h3>
                         </Fade>
-                        <Fade direction="right" triggerOnce>
+                        <Fade cascade direction="right" triggerOnce>
                             <ul className="text-lg list-disc pl-5">
                                 <li>Access to a large number of potential backers</li>
                                 <li>Opportunity to test the market before launching a product</li>
@@ -47,6 +45,17 @@ const SuccessStories = () => {
                         </Fade>
                     </div>
                 </div>
+                <Fade direction="up" triggerOnce>
+                    <div className="mt-10 text-center">
+                        <p className="text-lg mb-4">At CrowdCube, we bring whether it’s creative ideas, business ventures, startups, or even solving personal issues, our platform empowers people to turn ambitions into reality. Together, we can back initiatives that matter and make a lasting impact. If our mission inspires you, we invite you to explore our campaigns and to form a brighter future.</p>
+                        <a
+                            href="/campaigns"
+                            className="inline-block px-3 py-3 bg-purple-900 text-white font-semibold rounded-lg shadow-md hover:bg-purple-700 transition duration-300"
+                        >
+                            View All Campaigns
+                        </a>
+                    </div>
+                </Fade>
             </div>
         </section>
     );
